76532b3edf64397e5ab53bcce347cff9da7a7145
Samvel Avanesov <seavan@gmail.com>
2016-09-10 16:38:39 -0400

    fix for ios 10

--- src/ios/Open.m
+++ src/ios/Open.m
@@ -61,7 +61,9 @@ typedef void (^AlertViewCompletionBlock)(NSInteger buttonIndex);
           
         [previewCtrl.navigationItem setRightBarButtonItem:nil];
           
-        [self.viewController presentViewController:previewCtrl animated:YES completion:nil];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.viewController presentViewController:previewCtrl animated:YES completion:nil];
+            });
 
         NSLog(@"cordova.disusered.open - Success!");
         commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
